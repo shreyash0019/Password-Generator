c1b05f537980c073fe18382007b53fefabfbc66d
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-02-09 14:11:49 +0530

    main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import string
 total = string.ascii_letters + string.digits + string.punctuation
 length = int(input("Enter a length of password: "))
 password = "".join(random.choices(total, k=length))  
-print(password)
+print("Generated Password: {} ".format{password})
